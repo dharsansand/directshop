6652bad79d8e699e97f029e6e751d39c888c326d
dharsansand <dharsansand@gmail.com>
2024-11-10 17:30:07 +0530

    header

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,13 @@
-
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
-        {/* Set the title for the page */}
-        <title>directshop</title>
-        
-        {/* Add Favicon */}
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Directshop</title>
         <link rel="icon" href="/favicon.ico" />
-        </head>
-      <body >
-        {children}
-      </body>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
